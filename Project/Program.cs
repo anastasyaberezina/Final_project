173d5b2d63de2f00720781a02b709a16923510de
Anastasya <gavrilovaanastasya@yandex.ru>
2022-11-13 12:34:59 +0300

    add program second

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,14 +1,12 @@
 ﻿// Из имеющегося массива строк формирует массив из строк, длина которых меньше, либо равна 3 символа.//
 
-/*
-
-string text = "Должен вам сказать, что, мы вовсе не хотим завоевывать никакой Космос."   //Ввела некий текст
+/*string text = "Должен вам сказать, что, мы вовсе не хотим завоевывать никакой Космос."   
             + "Мы хотим расширить Землю до его границ. Мы не знаем, что делать с иными"
             + "мирами. Нам не нужно других миров. Нам нужно зеркало… Мы бьёмся над"
             + "контактом и никогда не найдём его. Мы в глупом положении человека,"
             + "рвущегося к цели, которой он боится, которая ему не нужна. Человеку нужен человек!";
 
-string[] text2 = text.Split(' ', '.', ',', '!'); //Разбила текст на подстроки с исключением символов
+string[] text2 = text.Split(' ', '.', ',', '!'); 
 
 foreach (var word in text2)  
 {
@@ -17,3 +15,31 @@ foreach (var word in text2)
     Console.Write($"{word+", "}"); 
     }
 }*/
+
+
+string[] arraywords = new string[11]{"Должен", "вам", "сказать", "что", "мы", "вовсе", "не", "хотим", "завоевывать", "никакой", "Космос"};
+string[] arraywords2 = new string[arraywords.Length];
+ArrayResult(arraywords, arraywords2);
+PrintArray(arraywords2);
+
+void ArrayResult (string[] arraword, string[] arraword2)
+{
+    int index = 0;
+    for(int i = 0; i<arraword.Length; i++)
+    {
+        if((arraword[i].Length<=3)&&(arraword[i].Length>=1))
+        {
+         arraword2[index]=arraword[i];
+         index++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]} ");
+    }
+    Console.WriteLine();
+}
